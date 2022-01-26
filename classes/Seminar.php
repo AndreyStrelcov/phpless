c45868d384cd5b84dc9ae2379436d9d64b9e53dc
Andrew Strelcov <astrelcov@yahoo.com>
2022-01-26 16:10:33 +0200

    composition

---
+++ classes/Seminar.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace classes;
+
+class Seminar extends Lesson
+{
+
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once "classes/Lesson.php";
+include_once "classes/Lecture.php";
+include_once "classes/Seminar.php";
+
+
+
+
+
+  
