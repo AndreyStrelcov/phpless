6f370a51b51b143b55b960c21bbb5c1b75fd1b17
Andrew Strelcov <astrelcov@yahoo.com>
2022-01-31 12:44:55 +0200

    start lesson

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
 //error_reporting(-1);
 
 
-//require_once '/var/www/phpless/classes/WordFactory.php';
-include_once "classes/WordFactory.php";
-
-
-$hello = new WordFactory();
-
-$hello->hello();
-
 
 
 
