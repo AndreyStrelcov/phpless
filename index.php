7567a7c1f8a77bbaff42c91c6c20523bd9072641
Andrew Strelcov <astrelcov@yahoo.com>
2022-02-01 00:45:28 +0200

    end lesson

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 
 //error_reporting(-1);
 
+/*
+include_once '/var/www/phpless/data.php';
+echo $a;
+echo $y;*/
+
+/*$path = __DIR__ . '/data.php';
+echo $path;*/
 
 
+//declare(strict_types=1);
 
+class A
+{
+    public $x;
+}
+
+$a = new A;
+
+function dump(int $x): string
+{
+    return $x;
+}
+
+var_dump(dump(5));
 
 
 
